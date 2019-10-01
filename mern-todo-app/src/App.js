c368c40abdd3a99de367a1b1071f4bc8af389313
Matt Anderson <matt.anderson.inf@gmail.com>
2019-09-30 23:58:52 -0400

    Created primary react component files
    Created primary react component files

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 // import bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
 class App extends Component{
   render(){
     return (
@@ -12,10 +16,10 @@ class App extends Component{
       <Router>
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
