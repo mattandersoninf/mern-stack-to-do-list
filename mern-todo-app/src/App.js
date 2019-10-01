ed6ccd61a7ffbc56cfaa3855936f43f1286e093a
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-01 02:30:42 -0400

    Create-todo component complete along with most of the FE functionality
    Create-todo component complete along with most of the FE functionality

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -4,9 +4,13 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 // import bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// import the create-todo, edit-todo, and todos-list component files
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+
+// import the logo png file
+import logo from './logo-og.png';
 
 class App extends Component{
   render(){
@@ -15,7 +19,23 @@ class App extends Component{
       // The <Route> elements will point to component files. The App calls the component files and the component files cutomize the components as needed/written
       <Router>
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              {/* The "logo" img is being referenced to the logo.svg file */}
+              <img src={logo} width="55" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-link">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="nav-link">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,10 +1,121 @@
 import React, {Component} from 'react';
 
 export default class CreateTodo extends Component{
-    render(){
-        return(
-            <div>
-                <p>Welcome to Create Todo Component!!</p>
+    // this is the constructor method that is the default state for responsibilities in the todo list.
+    // the description, responsible, and priority parameters are initally blank and the completed status is initially false
+    constructor(props){
+        super(props);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+
+        // these lines bind the functions below to this constructor so that these functions can be used to manipulate todo items
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    // these are the updating methods that will be used to alter the state of the responsibility on the todo list
+    onChangeTodoDescription(e){
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+    onChangeTodoResponsible(e){
+        this.setState({
+            todo_responsible: e.target.value
+        })
+    }
+    onChangeTodoPriority(e){
+        this.setState({
+            todo_priority: e.target.value
+        })
+    }
+    // this is the method that will handle the submit event of the form which will be implemented to create a new todo item
+    onSubmit(e){
+        // this line ensures that the default html behavior does not occur
+        // we're just recording what's going on in the log because the server has not been set up yet
+        e.preventDefault();
+        console.log(`Form submitted:`);
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+        
+        this.setState({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+    }
+    // this is the JSX code that will render the form for the todo list
+    render() {
+        return (
+            <div style={{marginTop: 10}}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    {/* There is the option to use a fieldset but granted this form does not need to be separated into sets, there's no need to add fieldsets */}
+                    <div className="form-group"> 
+                        <label>Description: </label>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.todo_responsible}
+                                onChange={this.onChangeTodoResponsible}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityLow" 
+                                    value="Low"
+                                    checked={this.state.todo_priority==='Low'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityMedium" 
+                                    value="Medium" 
+                                    checked={this.state.todo_priority==='Medium'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityHigh" 
+                                    value="High" 
+                                    checked={this.state.todo_priority==='High'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
