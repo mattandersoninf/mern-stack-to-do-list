c7ad6d4f0c92b08a9679ea5e2aea8d2f67c72aa6
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-02 01:24:12 -0400

    Set up todo model schema, and added endpoint functions
    Set up todo model schema(todo.model.js), and added endpoint functions (see the todoRoutes in server.js)

--- mern-todo-app/backend/server.js
+++ mern-todo-app/backend/server.js
@@ -1,19 +1,66 @@
 // Server.js
 
-// Express objject set up
+// Express object set up and call express library
 const express = require('express');
 const app = express();
 // Body-Parser object
 const bodyParser = require('body-parser');
-// CORS object
+// call the cors library
 const cors = require('cors');
+// call the mongoose library
+const mongoose = require('mongoose');
 // Server port to access the node database
 const PORT = 4000;
+// connect the schema to your express router
+const todoRoutes = express.Router();
 
-// Tell express to use the cors package
+
+// Tell the server to use the cors library
 app.use(cors());
-// Tell express to use the bodyParser witht the format for JSON objects
+// Tell the server to use the bodyParser library with the format for JSON objects
 app.use(bodyParser.json())
+// Tell the server to use the express router (your middleware that will start taking control of request starting with path '/todos')
+app.use('/todos', todoRoutes);
+
+
+// Add an endpoint which is grabbing all available todos items, you feed your function a request argument and a result argument
+todoRoutes.route('/').get(function(req,res){
+    
+    // Here is your error handling    
+    todoRoutes.find(function(err,todos){
+
+        if(err){
+            // if an error has occured, log it in the console
+            console.log(err);
+        }else{
+            // grab your results in json form
+            res.json(todos);
+        }
+    
+    });
+});
+
+
+// Add another endpoint used to get a todo item by feeding it an id
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+
+
+
+// Set up the connection to the mongodb server 
+// "mongodb://127.0.0.1:27017/todos" is the default mongo path
+mongoose.connect('mongodb://127.0.0.1:27017/todos', {useNewUrlParser:true});
+const connection = mongoose.connection;
+
+// Do a one time console log so that you know that you're server is set up
+connection.once('open',function(){
+    console.log("MongoDB database connection established successfully!");
+})
 
 // Have node server setup on the port object from above and return the string that says what port it is established on
 app.listen(PORT, function(){
---
+++ mern-todo-app/backend/todo.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// The schema make up is as follows: at first glance, it is comparable to a single MySQL table
+let Todo = new Schema({
+    todo_description:{
+        type: String
+    }
+    todo_responsible: {
+        type: String
+    },
+    todo_priority: {
+        type: String
+    },
+    todo_completed: {
+        type: Boolean
+    }
+})
+
+// Establish a mongoose model that you can create, read, update, delete, and store data from based on teh schema written above
+module.exports = mongoose.model('Todo', Todo);
