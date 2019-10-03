0758c1f10a9f38e57d272d72a440dd77ec621b37
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-03 00:10:16 -0400

    Finalized main parts of MERN todo app
    Finalized main parts of MERN todo app, still need styling, more error handling, folder expansion, hiding todo items or separating incomplete and complete

--- mern-todo-app/src/components/edit-todo.component.js
+++ mern-todo-app/src/components/edit-todo.component.js
@@ -1,10 +1,166 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
 
 export default class EditTodo extends Component{
+
+    constructor(props) {
+        super(props);
+        
+        // Set default values of the todo item you want to edit to it's current state
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onChangeTodoCompleted = this.onChangeTodoCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        // Set the states of the values that will be set to update initially to null strings or false
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    todo_description: response.data.todo_description,
+                    todo_responsible: response.data.todo_responsible,
+                    todo_priority: response.data.todo_priority,
+                    todo_completed: response.data.todo_completed
+                })   
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    // Change the states of the DESCRIPTION value of todo items
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+
+    // Change the state of the RESPONSIBLE value of todo items
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    // Change the state of the PRIORITY value of todo items
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+
+    // Change the states of the COMPLETED value of todo items
+    onChangeTodoCompleted(e) {
+        this.setState({
+            todo_completed: !this.state.todo_completed
+        });
+    }
+
+    // submit the changes to the updated todo item
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+        console.log(obj);
+        axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+            .then(res => console.log(res.data));
+        
+        this.props.history.push('/');
+    }
+
+    // put together the html for the edit page
     render(){
         return(
             <div>
-                <p>Welcome to Edit Todo Component!!</p>
+                <h3 align="center">Update Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <label>Description: </label>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.todo_responsible}
+                                onChange={this.onChangeTodoResponsible}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityLow" 
+                                    value="Low"
+                                    checked={this.state.todo_priority==='Low'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityMedium" 
+                                    value="Medium" 
+                                    checked={this.state.todo_priority==='Medium'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityHigh" 
+                                    value="High" 
+                                    checked={this.state.todo_priority==='High'} 
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-check">
+                        <input  className="form-check-input"
+                                id="completedCheckbox"
+                                type="checkbox"
+                                name="completedCheckbox"
+                                onChange={this.onChangeTodoCompleted}
+                                checked={this.state.todo_completed}
+                                value={this.state.todo_completed}
+                                />
+                        <label className="form-check-label" htmlFor="completedCheckbox">
+                            Completed
+                        </label>                        
+                    </div>
+
+                    <br />
+
+                    <div className="form-group">
+                        <input type="submit" value="Update Todo" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
-const Todo = props =>(
+// Page where you can edit the todo items
+const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
         <td>
             <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
